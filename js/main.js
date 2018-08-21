115aede75b5c25425b14fae3717ec6d20fbefd76
WhitexSC <whitex1337@gmail.com>
2018-08-22 00:00:57 +0300

    dropmenu height & navigation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
+// DROPDOWN MENU
 $( "#hamburger" ).on( "click", function() {
+   $('#hidden_height').height($('#content').height());
   if ($("#hamburger").hasClass("active_burger")) {
     $("#hamburger").removeClass("active_burger");
-     $(".container-fluid.hidden").hide();
+     $("#hidden").hide();
      $("body").removeClass("menuOpened")
   }
   else{
     $( "#hamburger").addClass("active_burger");
-     $(".container-fluid.hidden").show();
+     $("#hidden").show();
      $("body").addClass("menuOpened")
   }
 }); 
+
+// NAVIGATION
+var side_actmenu = $('.sidenav__item.active');
+var side_menu = $('.sidenav__item');
+
+$(side_menu).click(function(){
+  if(!$(this).hasClass('active')){
+    $(side_actmenu).removeClass('active');
+    $(this).addClass('active');
+    side_actmenu = $(this);
+  };
+});
